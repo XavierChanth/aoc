6c8f6d8a4ede16400ca46da8669b902b82ec750e
xavierchanth <xchanthavong@gmail.com>
2023-12-05 17:38:07 -0500

    add day 1/2 progress

---
+++ day1/day1.go
@@ -0,0 +1,89 @@
+package day1
+
+func getDigit(char byte) (isDigit bool, num int) {
+  digit := int(char - '0')
+  if digit < 0 || digit > 9 {
+    return false, 0
+  }
+  return true, digit
+}
+
+func Q1(input []string) (res int, err error) {
+	res = 0
+
+	for _, line := range input {
+		i := 0  
+    
+    for i < len(line) {
+			isDigit, num := getDigit(line[i])
+      if(isDigit) {
+        res += 10*num
+        break
+      }
+			i+=1
+		}
+    
+    i = len(line)-1
+    for i >= 0 {
+      isDigit, num := getDigit(line[i])
+      if(isDigit) {
+        res += num
+        break
+      }
+      i-=1
+    }
+
+	}
+
+	return res, err
+}
+
+func Q2(input []string) (res int, err error) {
+  res = 0
+  words := [...]string {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+
+  for _, line := range input {
+    i := 0  
+
+    outer:
+    for i < len(line) {
+      isDigit, num := getDigit(line[i])
+      if(isDigit) {
+        res += 10*num
+        break
+      }
+
+      for pos, word := range words {
+        if(i+len(word) <= len(line) && line[i:i+len(word)] == word) {
+          res += 10 * (pos+1)
+          break outer
+        }
+      }
+      
+      i+=1
+    }
+
+    i = len(line)-1
+    
+    outer2:
+    for i >= 0 {
+      isDigit, num := getDigit(line[i])
+      if(isDigit) {
+        res += num
+        break
+      }
+      
+      for pos, word := range words {
+        if(i+len(word) <= len(line) && line[i:i+len(word)] == word) {
+          res += pos+1
+          break outer2
+        }
+      }
+     
+      i-=1
+    }
+
+  }
+
+  return res, err
+}
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+  "fmt"
+  "main/input"
+  d "main/day2"
+)
+
+func main() {
+  lines, err := input.Readlines("day2/input.txt")
+  if err != nil {
+    fmt.Println(err)
+  }
+  res, err := d.Q1(lines)
+  if err != nil {
+    fmt.Println(err)
+  }
+  fmt.Println("Result: ", res)
+}
